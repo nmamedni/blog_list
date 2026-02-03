c25f51bb36e8776976e281ae856d4f911ca817ee
Natali Mamedniyazova <natali.mamedniyazova@gmx.de>
2026-02-03 11:39:19 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config =  require('./utils/config')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
